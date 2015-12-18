574f9af519300e1e943d2eb2848e94fb0eb1a9c8
abhishekg785 <abhishekg785@gmail.com>
2015-12-18 15:20:17 +0530

    all extra route removed

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/events',events);
-app.use('/addUser',addUser);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
